081592ffa54e416b0d859758f2b73bfd45b21e56
integreator <walterfrontier@gmail.com>
2020-06-12 00:11:31 -0400

    user input section complete

--- src/app/pages/angular-doc/fundamentals/comps-temps/comps-temps-routing.module.ts
+++ src/app/pages/angular-doc/fundamentals/comps-temps/comps-temps-routing.module.ts
@@ -3,6 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { CompsTempsComponent } from './comps-temps.component';
 import { DisplayDataComponent } from './display-data/display-data.component';
 import { TemplateSyntaxComponent } from './template-syntax/template-syntax.component';
+import { UserInputComponent } from './user-input/user-input.component';
+import { AttributeDirectivesComponent } from './attribute-directives/attribute-directives.component';
 
 const routes: Routes = [
   {
@@ -11,6 +13,8 @@ const routes: Routes = [
     children: [
       { path: 'display-data', component: DisplayDataComponent },
       { path: 'template-syntax', component: TemplateSyntaxComponent },
+      { path: 'user-input', component: UserInputComponent },
+      { path: 'attribute-directives', component: AttributeDirectivesComponent },
     ],
   },
 ];
--- src/app/pages/angular-doc/fundamentals/comps-temps/user-input/user-input.component.ts
+++ src/app/pages/angular-doc/fundamentals/comps-temps/user-input/user-input.component.ts
@@ -3,13 +3,37 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-user-input',
   templateUrl: './user-input.component.html',
-  styleUrls: ['./user-input.component.css']
+  styleUrls: ['./user-input.component.css'],
 })
 export class UserInputComponent implements OnInit {
+  inputValue1: string = '';
+  inputValue3: string = '';
+  inputValue4: string = '';
+  inputValue6: string[] = [];
+  inputKeyValue1: string = '';
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  handleClick() {
+    alert(`you clicked me. `);
   }
-
+  handleKeyupV1(event: any) {
+    this.inputValue1 = event.target.value;
+    this.inputKeyValue1 = event.key;
+  }
+  handleKeyupV2(event: KeyboardEvent) {
+    this.inputValue1 = (event.target as HTMLInputElement).value;
+  }
+  handleKeyupV3(value: string) {
+    this.inputValue3 = value;
+  }
+  handleKeyupV4(value: string) {
+    this.inputValue4 = value;
+  }
+  handleClickV6(value: string) {
+    if (value) {
+      this.inputValue6.push(value);
+    }
+  }
 }
